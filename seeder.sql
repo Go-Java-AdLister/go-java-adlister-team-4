bc8a3b402648a4d671d335c32e67e45281cd2f8f
Nia Watson <nia.d.watson@gmail.com>
2022-07-21 10:06:47 -0500

    seeder file

---
+++ seeder.sql
@@ -0,0 +1,8 @@
+USE adlister_db;
+
+INSERT INTO ads (user_id, category, plant_name, location, price, description)
+VALUES ('1', 'indoor', 'Aloe', 'Arlington','50', 'small indoor plant'),
+       ('1', 'outdoor', 'Lemon Tree', 'Arlington','75', 'large potted plant');
+
+
+
